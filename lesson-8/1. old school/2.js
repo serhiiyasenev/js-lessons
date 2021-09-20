b4100728c48959592b25bcd71b107d95212cff21
serhiiyasenev <serhii.yasenev@gmail.com>
2021-09-20 13:25:01 +0300

    Add files via upload

---
+++ lesson-8/1. old school/2.js
@@ -0,0 +1,14 @@
+var request = require('request');
+
+var options = {
+  'method': 'GET',
+  'url': 'https://del.qx.i-dealprospectus.com/aspx/Login',
+  'headers': {
+    'Host': 'del.qx.i-dealprospectus.com'
+  }
+};
+
+request(options, function (error, response) {
+  if (error) console.log(error);
+  console.log(response.statusCode);
+});
